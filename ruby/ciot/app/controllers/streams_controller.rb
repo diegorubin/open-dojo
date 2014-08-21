d3ec8b62bf28cb6b750afff310bde4e33867840b
Leandro L. Isotton <pequenoo@gmail.com>
2014-08-21 12:56:46 -0300

    Ciot: Route to list last 100 entries

--- ruby/ciot/app/controllers/streams_controller.rb
+++ ruby/ciot/app/controllers/streams_controller.rb
@@ -4,6 +4,13 @@ class StreamsController < ApplicationController
   def new
     key = request.headers['key']
     device = Device.find_by_key(key) if key
+
+    # @TODO: verify if json is valid.
+    #begin
+    #  !!JSON.parse(key)
+    #rescue
+    #  render status: :bad_request
+    #end
     
     if device
       device.streams.create(body: request.body.read)
@@ -11,5 +18,28 @@ class StreamsController < ApplicationController
     else
       render status: :bad_request
     end
+  end
+
+  def list
+    id = params[:id]
+
+    begin
+      device = Device.find(id) if id
+      
+      @streams = device.streams.select(:body,:created_at,:id).limit(100).map do |stream|
+        stream.body = JSON.parse(stream.body)
+        stream
+      end
+
+      render json: @streams
+    rescue ActiveRecord::RecordNotFound
+      render json: {}
+    end
+
+    
+    #respond_to do |format|
+    #   format.json { render json: @device.errors }
+    #end
+
   end
 end
